ab62f2e226e5a19559e3bd09b2e45d02253c1e6a
Mahimkar Saad Tahzeeb <117567622+SaadMahi@users.noreply.github.com>
2023-09-29 15:48:13 +0530

    ✨ feat: user input extraction on console

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,12 +1,19 @@
 import "../Form/Form.css";
 
 const Form = function () {
+
+    // user input excrator function
+    const userinput = function(user, value){
+        console.log(user, value)
+    }
+
+
   return (
     <form id="form-container">
       <span>Username</span>
-      <input id="user-name-input" type="text" placeholder="Enter name"></input>
+      <input onChange={e => userinput(e.target.id, e.target.value)} id="user-name-input" type="text" placeholder="Enter name"></input>
       <span>Age(years)</span>
-      <input id="user-age-input" type="number" placeholder="Enter Age"></input>
+      <input onChange={e => userinput(e.target.id, e.target.value)} id="user-age-input" type="number" placeholder="Enter Age"></input>
 
       <div className="btn-container">
         <button>Add user</button>
