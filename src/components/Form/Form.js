056a34c78b3664a0c2ef924a532bb475a39cf1c3
Mahimkar Saad Tahzeeb <117567622+SaadMahi@users.noreply.github.com>
2023-09-29 16:32:32 +0530

    📝 feat: updating user input to obj and log to console

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,22 +1,42 @@
+import { useState } from "react";
 import "../Form/Form.css";
 
 const Form = function () {
 
+  const initialInput = {
+    'user-name-input': '',
+    'user-age-input': ''
+  }
+
+    // useState to store values and display on Ui
+   const [defInputs, setInput] = useState(initialInput)
+
     // user input excrator function
-    const userinput = function(user, value){
-        console.log(user, value)
+    const userInput = function(user, value){
+        // console.log(user, value)
+        setInput(((prev) => ({
+          ...prev,
+          [user]: value
+        })))
     }
+
+    // form submit handler
+const formSubmit = function(e){
+  e.preventDefault();
+  console.log(defInputs)
+  setInput(initialInput)
+}
 
 
   return (
-    <form id="form-container">
+    <form onSubmit={formSubmit}  id="form-container">
       <span>Username</span>
-      <input onChange={e => userinput(e.target.id, e.target.value)} id="user-name-input" type="text" placeholder="Enter name"></input>
+      <input onChange={e => userInput(e.target.id, e.target.value)} id="user-name-input" value={defInputs["user-name-input"]} type="text" placeholder="Enter name"></input>
       <span>Age(years)</span>
-      <input onChange={e => userinput(e.target.id, e.target.value)} id="user-age-input" type="number" placeholder="Enter Age"></input>
+      <input onChange={e => userInput(e.target.id, e.target.value)} id="user-age-input" value={defInputs["user-age-input"]} type="number" placeholder="Enter Age"></input>
 
       <div className="btn-container">
-        <button>Add user</button>
+        <button type='submit' >Add user</button>
       </div>
     </form>
   );
